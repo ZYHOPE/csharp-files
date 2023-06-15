74122c157744b2029075230eec2b5824ecafe721
ZYHOPE <126283329+ZYHOPE@users.noreply.github.com>
2023-06-15 19:11:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("On this day, July 15th, 2023. I have have began my journey through programming(kinda)");
