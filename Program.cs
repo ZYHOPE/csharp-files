f42f469851d38f2761205937bd549299c5f6a2e9
ZYHOPE <126283329+ZYHOPE@users.noreply.github.com>
2023-06-15 19:05:03 -0400

    Add files via upload
    This is my first hello world program

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Writing 'Console.Write' will printout the text without adding a new line");
+Console.Write("For example  " + 3 + 9);
+Console.Write(4 + 9);
